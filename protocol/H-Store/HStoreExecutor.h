fa567228d21b1c00ebf99a586f73ff1c5d7748b8
zxjcarrot <zxjcarrot@gmail.com>
2021-12-28 15:16:19 -0500

    Fix

--- protocol/H-Store/HStoreExecutor.h
+++ protocol/H-Store/HStoreExecutor.h
@@ -1829,6 +1829,7 @@ public:
           this->n_abort_lock.fetch_add(1);
         }
         handle_requests();
+        flush_grouped_messages();
       }
     }
     if (this->partitioner->replica_num() > 1 && is_replica_worker == false) {
