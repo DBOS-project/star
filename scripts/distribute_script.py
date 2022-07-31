ad76d537f096a1f3b33221bb3d4ce951f236e7ac
zxjcarrot <zxjcarrot@gmail.com>
2022-07-31 19:01:05 +0000

    Update

--- scripts/distribute_script.py
+++ scripts/distribute_script.py
@@ -9,11 +9,12 @@ outs = [line.split("\t")[1] for line in ips]
 
 port = int(sys.argv[1]) 
 script = sys.argv[2]
+gc_zone_name = sys.argv[3]
 script_base = os.path.basename(script)
 script_no_extension = os.path.splitext(script_base)[0]
 print(script_no_extension)
 for i in range(n):
   os.system("python %s %d %d > run.sh" % (script, i, port))
   os.system("chmod u+x run.sh")
-  os.system("gcloud compute scp --zone us-central1-a run.sh %s:/home/zxjcarrot/star/%s_run.sh" % (outs[i], script_no_extension))
+  os.system("gcloud compute scp --zone %s run.sh %s:~/star/%s_run.sh" % (gc_zone_name, outs[i], script_no_extension))
   #os.system("scp run.sh ubuntu@%s:~/star/run.sh" % outs[i])
